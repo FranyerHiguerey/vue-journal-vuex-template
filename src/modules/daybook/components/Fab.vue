f62f3d0f2b2dcbdd69a75c4092f3efdd206ce166
Franyer Higuerey <franyerhiguerey@gmail.com>
2021-11-20 02:52:22 -0700

    Commit para el primer template

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn btn-primary">
+      <i :class="['fa', 'fa-2x', icon]"></i>
+  </button>
+</template>
+
+<script>
+export default {
+  props:{
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button{
+    border-radius: 80%;
+    bottom: 20px;
+    height: 80px;
+    position: fixed;
+    right: 20px;
+    width: 80px;
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,20 @@
+export default () => ({
+    isLoading: true,
+    entries:[
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Eiusmod duis ad amet anim sunt nulla magna nisi velit in est. Exercitation commodo ex in proident incididunt eiusmod. Nostrud incididunt deserunt cupidatat nostrud cupidatat id aute esse nisi ex proident irure non ullamco.'
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Proident quis ipsum consequat reprehenderit enim reprehenderit incididunt labore esse laborum voluptate. Anim occaecat qui pariatur esse sunt. Ex deserunt sint deserunt veniam culpa ad exercitation veniam aute non dolore. Consequat ipsum exercitation elit reprehenderit exercitation ad ut sunt id laboris mollit dolor sit.'
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Mollit ea veniam sint fugiat nisi ut sit minim ea aute reprehenderit. Sint voluptate Lorem nostrud consequat ipsum. Magna esse magna adipisicing labore Lorem et cillum cillum laborum est proident eu. Id fugiat sunt esse labore magna ut Lorem aute do ullamco. Pariatur reprehenderit amet occaecat labore cillum labore nulla ut qui in non ipsum in nostrud.'
+        },
+    ]
+})
